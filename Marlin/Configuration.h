6617ffdbe30edde15209531f3a2d697f50a7a157
Greg Starr <marnoot@outlook.com>
2020-05-29 22:10:14 -0600

    UPdate Y offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -927,7 +927,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 5, -43, -0.5 }
+#define NOZZLE_TO_PROBE_OFFSET { 0.5, -39, -0.5 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
